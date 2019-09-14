a10739bb14f9e496220cf853dbb97e7492baa2a4
Americo Albuquerque <americo.l.albuquerque@gmail.com>
2019-09-14 08:00:25 +0200

    Add project files

---
+++ web-server/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace web_server
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string pathToContentRoot = Directory.GetCurrentDirectory();
+            IConfiguration configuration = CreateConfiguration(args, pathToContentRoot);
+            CreateWebHostBuilder(args, configuration).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args, IConfiguration config)
+        {
+            string pattern = @"\[(\w+)\]";
+            MatchEvaluator evaluator = new MatchEvaluator(Replacer);
+            return WebHost.CreateDefaultBuilder(args)
+                .UseWebRoot(Regex.Replace(config["web:root"], pattern, evaluator, RegexOptions.IgnoreCase, TimeSpan.FromSeconds(2)))
+                .UseKestrel()
+                .UseConfiguration(config)
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>();
+        }
+
+        public static IConfiguration CreateConfiguration(string[] args, string pathToContentRoot)
+        {
+            string environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+                .SetBasePath(pathToContentRoot)
+                .AddJsonFile($"appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{environmentName}.json", true, true)
+                .AddJsonFile($"load-manager.json", true, true)
+                .AddJsonFile($"load-manager.{environmentName}.json", true, true)
+                .AddEnvironmentVariables()
+                .AddCommandLine(args);
+
+            return builder.Build();
+        }
+
+        public static string Replacer(Match match)
+        {
+            if (match.Success)
+            {
+                if (match.Groups[1].Value.ToUpper() == "CWD")
+                {
+                    return Directory.GetCurrentDirectory();
+                }
+                return match.Groups[1].Value;
+            }
+            return match.Value;
+        }
+    }
+}
